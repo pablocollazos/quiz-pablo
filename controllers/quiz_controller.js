4a1545bce09d1fa70df0cc737a0918fba16694e0
Pablo Collazos Fernandez <pablo.collazos@gmail.com>
2015-08-18 18:27:24 +0200

    Incluido autoload y control de errores

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,50 @@
 var models = require('../models/models.js');
 
+//Autoload - factoriza el codigo si ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if (quiz){
+				// Ponemos la quiz encontrada en el request
+				req.quiz = quiz;
+				next();
+			}else{
+				next(new Error('No existe quizId='+quizId));
+			}
+		}
+	).catch(function(error){next(error);});
+}
+
+// GET /quizes
+exports.index = function(req, res){
+	models.Quiz.findAll()
+	.then(function(quizes){
+		res.render('quizes/index', {quizes: quizes});
+	})
+	.catch(function(error){next(error);});
+}
+
+//GET /quizes/:id
+exports.show = function(req, res){
+	// Ya no necesito hacer la busqueda por id ya que se hace en la
+	// funcion load.
+	// models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show',{quiz: req.quiz});
+	// });
+}
+
+
+//GET /quizes/:id/answer
+exports.answer = function(req, res){
+
+		if (req.query.respuesta === req.quiz.respuesta){
+			res.render('quizes/answer',{quiz: req.quiz, respuesta: 'Correcto!!!'});
+		}else{
+			res.render('quizes/answer', {quiz: req.quiz, respuesta: 'Incorrecto!!!'})
+		}
+};
+
+
 //GET /quizes/question
 exports.question = function(req, res){
 	// Modificamos para recuperar las preguntas de base de datos
@@ -10,21 +55,3 @@ exports.question = function(req, res){
 	//res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
-//GET /quizes/answer
-exports.answer = function(req, res){
-
-	models.Quiz.findAll().success(function(quiz){
-		if(req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer', {respuesta: 'Correcto!!!'});
-		}else{
-			res.render('quizes/answer', {respuesta: 'Incorrecto!!!'})
-		}
-	});
-
-	
-	// if(req.query.respuesta === 'Roma'){
-	// 	res.render('quizes/answer', {respuesta: 'Correcto!!!'});
-	// }else{
-	// 	res.render('quizes/answer', {respuesta: 'Incorrecto!!!'})
-	// }
-};
