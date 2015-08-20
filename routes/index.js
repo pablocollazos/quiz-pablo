661b1b37d02a0d93ce01640a1145292f73ae9d40
Pablo Collazos Fernandez <pablo.collazos@gmail.com>
2015-08-20 19:48:48 +0200

    Control de usuarios y de publicacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,20 +13,27 @@ router.get('/', function(req, res) {
 //Autoload de comandos con :quizId
 // Solo se ejecuta esto si en la URL llega quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Incluimos rutas para pregunta y respuesta
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
-router.get('/quizes/new', 					quizController.new);
-router.post('/quizes/create', 				quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', 	quizController.edit);
-router.put('/quizes/:quizId(\\d+)', 		quizController.update);
-router.delete('/quizes/:quizId(\\d+)', 		quizController.destroy);
+router.get('/quizes/new', 					sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', 				sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', 	sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired,commentController.publish);
 
 // router.get('/quizes/question', quizController.question);
 // router.get('/quizes/answer', quizController.answer);
